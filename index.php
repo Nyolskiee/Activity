d5d0d7e7b34b7554c101be6248a602b9f85c18ae
unknown <mondiaarniel4@gmail.com>
2024-10-06 00:00:33 +0800

    success

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hands on Act</title>
+    <link rel="stylesheet" href="style.css">
+   
+</head>
+<body>
+   <h1><?php
+   
+    $FirstName = "Bryson";
+    $LastName = "Geronimo";
+    $Age = "21";
+    $Address = "Cardona";
+    $Contact = "1";
+
+    $FName = "Andrew";
+    $LName = "Rodrigo";
+    $Ag = "21";
+    $Add = "Antipolo";
+    $Cont= "2";
+
+    $FN = "Arniel";
+    $LN= "Mondia";
+    $A = "21";
+    $Addr = "Angono rizal";
+    $Con = "3";
+
+    $F = "Reynald";
+    $L = "Boiser";
+    $Ages = "21";
+    $A = "Cardona";
+    $C = "4";
+?>
+
+
+<h3><?=$FirstName?></h3>
+<h3><?=$LastName?></h3>
+<h3><?=$Age?></h3>
+<h3><?=$Address?></h3>
+<h3><?=$Contact?></h3>
+<br>
+<h3><?=$FName?></h3>
+<h3><?=$LName?></h3>
+<h3><?=$Ag?></h3>
+<h3><?=$Add?></h3>
+<h3><?=$Cont?></h3>
+<br>
+<h3><?=$FN?></h3>
+<h3><?=$LN?></h3>
+<h3><?=$A?></h3>
+<h3><?=$Addr?></h3>
+<h3><?=$Con?></h3>
+<br>
+<h3><?=$F?></h3>
+<h3><?=$L?></h3>
+<h3><?=$Ages?></h3>
+<h3><?=$A?></h3>
+<h3><?=$C?></h3>
+</body>
+</html>
